77df027434ae4109c8279a6a6cfd3f2d8955fd96
DarkTXYZ <pawaretmontfort@gmail.com>
2021-07-29 14:11:19 +0700

    update

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ var authorText = author.innerText
 
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
-const search = document.querySelector("#search");
+const search_btn = document.querySelector("#search");
+const reset_btn = document.querySelector("#reset");
+const text = document.querySelector('#text')
 
 btn_toggle.onclick = () => {
   // your code here
@@ -18,7 +20,7 @@ btn_toggle.onclick = () => {
     btn_toggle.innerText = "Display Author"
     toggle = true
   }
-  else{
+  else {
     author.innerText = authorText
     btn_toggle.innerText = "Display Calculation"
     toggle = false
@@ -26,4 +28,27 @@ btn_toggle.onclick = () => {
 
 }
 
+search_btn.onclick = () => {
+  /*
+  var l = length.value
+  var words = text.innerText.split(" ")
+  var selectword = []
+
+  for (const word of words) {
+    if (word.length >= l)
+      selectword.push(word)
+  }
+  for (const word of selectword) {
+    var innerHTML = text.innerHTML
+    var index = innerHTML.indexOf(word)
+    innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
+    text.innerHTML = innerHTML;
+  }
+  */
+}
+
+
+reset_btn.onclick = () => {
+  location.reload()
+}
 // more codes for Search and Reset buttons here
