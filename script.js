0d7ed5d53b08803dfe78d801f9ee079832c73ec8
DarkTXYZ <pawaretmontfort@gmail.com>
2021-07-29 13:58:52 +0700

    update

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const author = document.querySelector("#author")
 const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
+var toggle = false
+var authorText = author.innerText
+
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
-
-// define more constants and variables here
+const search = document.querySelector("#search");
 
 btn_toggle.onclick = () => {
   // your code here
+  if (!toggle) {
+    var l = length.value
+    var code = parseInt(author.innerText.split(" ")[0])
+    console.log(code - l)
+    author.innerText = code - l
+    btn_toggle.innerText = "Display Author"
+    toggle = true
+  }
+  else{
+    author.innerText = authorText
+    btn_toggle.innerText = "Display Calculation"
+    toggle = false
+  }
+
 }
 
 // more codes for Search and Reset buttons here
